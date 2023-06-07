c2453695553370637804a5f8ed638bbfa96f84ee
AdhamElagaty <adhamalagaty@gmail.com>
2023-06-08 01:00:50 +0300

    Create Person Class

--- Person.cpp
+++ Person.cpp
@@ -31,3 +31,40 @@ Person::Person(string id, string first_name, string second_name, string password
         throw invalid_argument("Invalid password");
     }
 }
+
+void Person::set_first_name(string first_name) {
+    this->first_name = first_name;
+}
+
+void Person::set_second_name(string second_name) {
+    this->second_name = second_name;
+}
+
+void Person::set_name(string first_name, string second_name) {
+    this->first_name = first_name;
+    this->second_name = second_name;
+}
+
+void Person::set_password(std::string password) {
+    this->password = password;
+}
+
+string Person::get_first_name() {
+    return this->first_name;
+}
+
+string Person::get_second_name() {
+    return this->second_name;
+}
+
+string Person::get_name() {
+    return (this->first_name + " " + this->second_name);
+}
+
+string Person::get_password() {
+    return this->password;
+}
+
+string Person::get_id() {
+    return this->id;
+}
--- Person.h
+++ Person.h
@@ -8,4 +8,13 @@ public:
     Person();
     Person(string first_name, string second_name, string password);
     Person(string id, string first_name, string second_name, string password);
+    void set_first_name(string first_name);
+    void set_second_name(string second_name);
+    void set_name(string first_name, string second_name);
+    void set_password(string password);
+    string get_first_name();
+    string get_second_name();
+    string get_name();
+    string get_password();
+    string get_id();
 };
